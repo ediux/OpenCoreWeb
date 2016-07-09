ee6b570d7bddc95784f4fb94437e800d12a84fc7
ediux <ediux@msn.com>
2016-07-10 02:22:18 +0800

    * IUnitofWork.cs: 加入Unit of Work 介面定義，以提供跨儲存庫的存取。
    * My.Core.Infrastructures.csproj: 加入 Base/IUnitofWork.cs 檔案至專案檔。

---
+++ My.Core.Infrastructures/Base/IUnitofWork.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace My.Core.Infrastructures
+{
+	public interface IUnitofWork : IDisposable
+	{
+		/// <summary>
+		/// 取得資料庫物件執行個體。
+		/// </summary>
+		/// <returns>The database object.</returns>
+		/// <typeparam name="TDb">The 1st type parameter.</typeparam>
+		TDb GetDatabaseObject<TDb>() where TDb : class;
+		/// <summary>
+		/// Gets the entity.
+		/// </summary>
+		/// <returns>The entity.</returns>
+		/// <typeparam name="TDbSet">The 1st type parameter.</typeparam>
+		TDbSet GetEntity<TDbSet>() where TDbSet : class;
+		/// <summary>
+		/// Begins the transcation.
+		/// </summary>
+		/// <returns>The transcation.</returns>
+		void BeginTranscation();
+		/// <summary>
+		/// Saves the changes.
+		/// </summary>
+		/// <returns>The changes.</returns>
+		void SaveChanges();
+		/// <summary>
+		/// Commits the transcation.
+		/// </summary>
+		/// <returns>The transcation.</returns>
+		void CommitTranscation();
+		/// <summary>
+		/// Closes the database.
+		/// </summary>
+		/// <returns>The database.</returns>
+		void CloseDatabase();
+	}
+}
+

44112851e99a725de8ea0cf91e0daaf998fe6c3b
ediux <ediux@msn.com>
2016-07-14 18:39:44 +0800

    * My.Core.Infrastructures/Repositories/IUserOperationCodeDefineRepository.cs:   增加操作紀錄代碼對應表存取層介面。
    * My.Core.Infrastructures/Repositories/IUserOperationLogRepository.cs:
      加入使用者操作紀錄資料表存取層介面。

---
+++ My.Core.Infrastructures/Repositories/IUserOperationCodeDefineRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using My.Core.Infrastructures.DAL;
+
+namespace My.Core.Infrastructures
+{
+	public interface IUserOperationCodeDefineRepository : IRepositoryBase<IUserOperationCodeDefine>
+	{
+		/// <summary>
+		/// Finds the by code.
+		/// </summary>
+		/// <returns>The by code.</returns>
+		/// <param name="code">Code.</param>
+		IUserOperationCodeDefine FindByCode(int code);
+	}
+}
+

926b2001a1a321774d053472d9ba028b33ccf1d0
ediux <ediux@msn.com>
2016-08-04 10:08:35 +0800

    * IDynamicLoadingforDIFrameworkService.cs: * IDynamicLoadingforDIFrameworkRepository.cs: 1.加入注入式相依性主機參考屬性。
    * My.Core.Infrastructures.csproj: 1.加入商業邏輯層服務類別目錄。

--- My.Core.Infrastructures/IoC/IDynamicLoadingforDIFrameworkRepository.cs
+++ My.Core.Infrastructures/IoC/IDynamicLoadingforDIFrameworkRepository.cs
@@ -1,8 +1,13 @@
-﻿using System;
-namespace My.Core.Infrastructures
+﻿namespace My.Core.Infrastructures.IoC
 {
-	public interface IDynamicLoadingforDIFrameworkRepository
+	public interface IDynamicLoadingforDIFrameworkRepository<TRepository> : IRepositoryBase<TRepository>
+		where TRepository:IDataModel
 	{
+		/// <summary>
+		/// 取得或設定注入式相依性主機執行個體。
+		/// </summary>
+		/// <value>The host.</value>
+		IDIHost Host { get; set; }
 	}
 }
 
--- My.Core.Infrastructures/IoC/IDynamicLoadingforDIFrameworkService.cs
+++ My.Core.Infrastructures/IoC/IDynamicLoadingforDIFrameworkService.cs
@@ -1,8 +1,12 @@
-﻿using System;
-namespace My.Core.Infrastructures
+﻿namespace My.Core.Infrastructures.IoC
 {
-	public interface IDynamicLoadingforDIFrameworkService
+	public interface IDynamicLoadingforDIFrameworkService : IServiceBase
 	{
+		/// <summary>
+		/// 取得或設定注入式相依性主機執行個體。
+		/// </summary>
+		/// <value>The host.</value>
+		IDIHost Host { get; set; }
 	}
 }
 

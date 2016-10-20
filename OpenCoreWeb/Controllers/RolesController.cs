d37f477a17073d64c398710fc646d581e0b4b325
ediux <ediuxcity@gmail.com>
2016-10-20 18:05:26 +0800

    [加入] 加入角色管理功能

---
+++ OpenCoreWeb/Controllers/RolesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using My.Core.Infrastructures.Implementations.Models;
+
+namespace OpenCoreWeb.Controllers
+{
+    public class RolesController : Controller
+    {
+        private OpenWebSiteEntities db = new OpenWebSiteEntities();
+
+        // GET: Roles
+        public ActionResult Index()
+        {
+            return View(db.ApplicationRole.ToList());
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationRole applicationRole = db.ApplicationRole.Find(id);
+            if (applicationRole == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationRole);
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Void,CreateUserId,CreateTime,LastUpdateUserId,LastUpdateTime")] ApplicationRole applicationRole)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ApplicationRole.Add(applicationRole);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(applicationRole);
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationRole applicationRole = db.ApplicationRole.Find(id);
+            if (applicationRole == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationRole);
+        }
+
+        // POST: Roles/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Void,CreateUserId,CreateTime,LastUpdateUserId,LastUpdateTime")] ApplicationRole applicationRole)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(applicationRole).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(applicationRole);
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationRole applicationRole = db.ApplicationRole.Find(id);
+            if (applicationRole == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationRole);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ApplicationRole applicationRole = db.ApplicationRole.Find(id);
+            db.ApplicationRole.Remove(applicationRole);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
